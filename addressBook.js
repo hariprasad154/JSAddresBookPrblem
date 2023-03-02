b61734ec6b9711f5b3a806b1f519ad2d41625379
hariprasad154 <hariprasas154@gmail.com>
2023-03-02 16:21:30 +0530

    added

--- addressBook.js
+++ addressBook.js
@@ -1,3 +1,6 @@
+// Adding contacts
+const prompt = require('prompt-sync')();
+
 let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
 let addressRegex = RegExp("^[A-Za-z]{4,}$");
 let cityStateRegex = RegExp("^[A-Za-z]{4,}$");
@@ -31,14 +34,49 @@ class AddressBook {
             this.email = params[7];
         else throw "Invalid Email!!";
     }
+}
 
-    toString() {
-        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
+
+let addressBookArr = new Array();
+
+let getContact = () => {
+    let firstName = prompt("Enter First Name ");
+    let lastName = prompt("Enter Last Name ");
+    let address = prompt("Enter Address ");
+    let city = prompt("Enter City ");
+    let state = prompt("Enter State ");
+    let zip = prompt("Enter Zip ");
+    let phoneNumber = prompt("Enter Phone Number ");
+    let email = prompt("Enter Email ");
+    let contactInput = null;
+
+    try {
+        contactInput = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+    } catch (error) {
+        console.error(error);
     }
+    return contactInput;
+};
+
+let addContact = (contact) => {
+    addressBookArr.push(contact);
+    console.log("Contact Added Successfully!!")
 }
-try {
-let addressBook = new AddressBook("hari", "Prasad", "Thane", "Mumbai", "Mahrashtra", "411057", "93839294848", "prasad@gmail.com");
-console.log(addressBook.toString());
-} catch(error) {
-    console.error(error);
-}
+
+console.log("Welcome to AddressBook Program!!");
+let choice = 0;
+
+do {
+    console.log("Choose\n1. Add Contact\n2. Exit");
+    choice = prompt("Enter Your Choice ");
+    switch (choice) {
+        case "1": addContact(getContact());
+            break;
+        case "2": 
+        console.log( "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email);
+        console.log("Thankyou!");
+        break;
+        default: console.log("Invalid Choice !!");
+    }
+
+} while (choice != 2)
